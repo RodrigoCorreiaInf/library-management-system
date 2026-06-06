ae225675d051c7c6363575e782cb3006ec44a7df
Rodrigo Correia <1220604@isep.ipp.pt>
2026-06-06 14:39:47 +0100

    init repo

---
+++ library-management-system/src/test/java/com/rodrigo/library_management_system/TestLibraryManagementSystemApplication.java
@@ -0,0 +1,11 @@
+package com.rodrigo.library_management_system;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestLibraryManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(LibraryManagementSystemApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
